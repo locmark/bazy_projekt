7e55a7eb01230cad69cb8a1b3170d45a254a1bd2
karol <kkraczyna@gmai.com>
2019-05-21 12:35:05 +0200

    triggers and functions

--- projekt.sql
+++ projekt.sql
@@ -1,4 +1,4 @@
-
+-- TABLES
 CREATE TABLE "Akademiki" (
     "id" serial   NOT NULL PRIMARY KEY,
     "nazwa" varchar(20)   NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE "Komputery" (
     "IP" varchar(20)   NOT NULL,
     "MAC" varchar(20)   NOT NULL,
     "id_studenta" int   NOT NULL,
-    "data_dodania" timestamp   NOT NULL
+    "data_dodania" timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE "Komputery_archiwum" (
@@ -35,7 +35,7 @@ CREATE TABLE "Komputery_archiwum" (
     "MAC" varchar(20)   NOT NULL,
     "id_studenta" int   NOT NULL,
     "data_dodania" timestamp   NOT NULL,
-    "data_archiwizacji" timestamp   NOT NULL
+    "data_archiwizacji" timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE "Pracownicy_akademika" (
@@ -57,6 +57,7 @@ CREATE TABLE "Zawody" (
     "nazwa" varchar(20)   NOT NULL
 );
 
+-- RELATIONS
 ALTER TABLE "Pokoje" ADD CONSTRAINT "fk_Pokoje_id_akademika" FOREIGN KEY("id_akademika")
 REFERENCES "Akademiki" ("id");
 
@@ -78,3 +79,28 @@ REFERENCES "Pracownicy" ("id");
 ALTER TABLE "Pracownicy" ADD CONSTRAINT "fk_Pracownicy_id_zawodu" FOREIGN KEY("id_zawodu")
 REFERENCES "Zawody" ("id");
 
+-- FUNCTIONS
+CREATE OR REPLACE FUNCTION archiwizacja_komputera_UPDATE() RETURNS TRIGGER
+LANGUAGE 'plpgsql'
+AS '
+ BEGIN
+    INSERT INTO "Komputery_archiwum"(host, IP, MAC, id_studenta, data_dodania) VALUES (OLD.host, OLD.IP, OLD.MAC, OLD.id_studenta, OLD.data_dodania);
+    RETURN NEW;
+ END;
+';
+
+CREATE OR REPLACE FUNCTION archiwizacja_komputera_DELETE() RETURNS TRIGGER
+LANGUAGE 'plpgsql'
+AS '
+ BEGIN
+    INSERT INTO "Komputery_archiwum"(host, IP, MAC, id_studenta, data_dodania) VALUES (OLD.host, OLD.IP, OLD.MAC, OLD.id_studenta, OLD.data_dodania);
+    RETURN NULL;
+ END;
+';
+
+-- TRIGGERS
+CREATE TRIGGER archiwizacja_komputera_UPDATE_TRIGGER BEFORE UPDATE ON "Komputery" FOR EACH ROW
+EXECUTE PROCEDURE archiwizacja_komputera_UPDATE();
+
+CREATE TRIGGER archiwizacja_komputera_DELETE_TRIGGER BEFORE DELETE ON "Komputery" FOR EACH ROW
+EXECUTE PROCEDURE archiwizacja_komputera_DELETE();
